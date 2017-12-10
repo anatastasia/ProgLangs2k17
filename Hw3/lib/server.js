3893450bb24866b0bdebd76b10c44aa0e3410635
anatastasia.f <anatastasia.f@gmail.com>
2017-12-11 00:58:32 +0300

    Getting there: added Xs and Os

--- Hw3/lib/server.js
+++ Hw3/lib/server.js
@@ -3,19 +3,40 @@ const nodeStatic = require("node-static");
 const path = require("path");
 const ws = require("ws");
 
+var marks = new Array('X', 'O')
+
 
 function handleWsConnection(clientWebSocket) {
+    var socketMark = 'Z';
+    if (marks.length) {
+            var mark = marks.pop();
+            socketMark = mark;
+            console.log("New client is " + mark);
+            clientWebSocket.send(JSON.stringify(
+                {
+                    "initMark": mark
+                }
+            ));
+    }
+
     const allClients = this.clients;
     console.log("Connected some client via web socket");
-    clientWebSocket.on("message", function (turnData) {
-       console.log("Got message: " + turnData);
-       for (var i=0; i<allClients.length; ++i) {
-           var client = allClients[i];
-           if (client != clientWebSocket) {
-               client.send(turnData);
-           }
-       }
-   });
+
+    clientWebSocket.on("message", function (messageData) {
+        console.log("Got message: " + messageData);
+        var parsedData = JSON.parse(messageData);
+        for (var i = 0; i < allClients.length; ++i) {
+            var client = allClients[i];
+
+            if (client != clientWebSocket) {
+                client.send(messageData);
+            }
+        }
+    });
+
+    clientWebSocket.on("close", function (code, reason) {
+        marks.push(socketMark);
+    });
 }
 
 
--- Hw3/public/client.js
+++ Hw3/public/client.js
@@ -2,21 +2,37 @@ const hosthame = window.location.hostname;
 const portNumber = window.location.port;
 const wsPath = "ws://"+hosthame+":"+portNumber+"/ws";
 const ws = new WebSocket(wsPath);
-ws.onmessage = function(data) {
-    var position = data.data.substr(5)
-    var cell = document.getElementById(position)
-    cell.innerText = "♞";
+var ownMark = "Z";
+
+ws.onmessage = function(messageData) {
+    var parsedData = JSON.parse(messageData.data);
+
+    if ("position" in parsedData) {
+        var position = parsedData["position"];
+        var opponentMark = parsedData["opponentMark"]
+        var cell = document.getElementById(position);
+        cell.innerText = opponentMark;
+    } else if ("initMark" in parsedData) {
+        ownMark = parsedData["initMark"];
+    }
 }
 
 function drawField() {
     for (var i = 0; i < 10; i++) {
-        var row = document.querySelector("table").insertRow(i)
+        var row = document.querySelector("table").insertRow(i);
+
         for (var j = 0; j < 10; j++) {
             var cell = row.insertCell(j);
             cell.id = String(i) + ":" + String(j);
+
             cell.addEventListener("click", function(event) {
-                this.innerText = "♞";
-                ws.send(["turn", this.id])
+                this.innerText = ownMark;
+                ws.send(JSON.stringify(
+                    {
+                        "position": this.id,
+                        "opponentMark": ownMark
+                    }
+                ));
             });
         }
     }
